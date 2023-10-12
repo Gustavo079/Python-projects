177a9099bb2c72d3f6d7a1ac168513f4c7e2730f
Gustavo079 <angusest90@gmail.com>
2023-10-12 12:47:37 -0600

    random number

---
+++ random-number/main.py
@@ -0,0 +1,14 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"guess a number between 1 & {x}: "))
+        if guess < random_number:
+            print("sorry,to low. guess again")
+        elif guess > random_number:
+            print("sorry, to high. Guess again")
+    print(f"Yeah, you guess the number {random_number}")
+
+guess(10)
